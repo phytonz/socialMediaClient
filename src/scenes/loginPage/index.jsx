c151333600c0919fa365a21f9bb1ae72ee587de1
Sakthiveel <sakthivelsaravanan636@gmail.com>
2023-04-04 11:49:48 +0530

    chages

--- src/scenes/loginPage/index.jsx
+++ src/scenes/loginPage/index.jsx
@@ -25,7 +25,7 @@ const LoginPage = () => {
         backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-          Welcome to SocialMedia, the Social Media for youth!
+          Welcome to SocialMedia !!!
         </Typography>
         <Form />
       </Box>
